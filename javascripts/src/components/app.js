a4cbe807b038f5dd4d4620f18386f29a8b250d43
Takkiii <s.takkiii.com@gmail.com>
2017-09-11 18:28:58 +0900

    :pencil2: Change component to simply

--- javascripts/src/components/app.js
+++ javascripts/src/components/app.js
@@ -1,10 +1,38 @@
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.data = {
+      users: [
+        {
+          id: 2,
+          name: 'にんじゃわんこ',
+          profilePicture: 'https://pbs.twimg.com/media/CvhteXTVIAAzHDw.jpg',
+        },
+        {
+          id: 3,
+          name: 'はらぺこ君',
+          profilePicture: 'https://pbs.twimg.com/profile_images/882917567743918080/pzpkX5DS_400x400.jpg',
+        },
+      ],
+      currentUser: {
+        id: 1,
+        name: 'Takkiii',
+        profilePicture: 'https://pbs.twimg.com/profile_images/899990905855156224/WVjZoIcl_bigger.jpg',
+      },
+    }
+  }
+
   render() {
+    const {
+      users,
+      currentUser
+    } = this.data
     return (
       <div className='app'>
         <Header />
-        <UserList />
-        <MessagesBox />
+        <UserList users={users} currentUser={currentUser}/>
+        <MessagesBox currentUser={currentUser}/>
       </div>
     )
   }
--- javascripts/src/components/replyBox.js
+++ javascripts/src/components/replyBox.js
@@ -1,14 +1,41 @@
 class ReplyBox extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      value: ""
+    }
+  }
+
+  handleChangeInput(e) {
+    this.setState({value: e.target.value})
+  }
+
+  handleKeyDown(e) {
+    // MessageStore的なものを用意する
+    // いつ？
+    // 誰が？
+    // 誰に？
+    // 同じデータを使いたい
+    // 同じデータを使う = データが更新されたらアプリケーションの全てのデータが同期される必要
+    // redux登場 reducer, action, store, connectなどの作成
+    if (e.key === 'Enter') {
+      console.log('pressed enter!!')
+    }
+  }
+
   render() {
+    const {value} = this.state
     return (
       <div className='reply-box'>
         <input
           className='reply-box__input'
-          placeholder='Type message to reply..'
+          placeholder='メッセージを入力'
+          value={value}
+          onChange={this.handleChangeInput.bind(this)}
+          onKeyDown={this.handleKeyDown.bind(this)}
         />
-        <span className='reply-box__tip'>
-          Press <span className='reply-box__tip__button'>Enter</span> to send
-        </span>
+        <span className='reply-box__tip'>Enterで送信</span>
       </div>
     )
   }
